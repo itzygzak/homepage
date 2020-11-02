5c675ba2f490262e78f4985a8c070e4d220c3fb7
Przemyslaw Duniec <pduniec@outlook.com>
2020-11-02 13:14:24 +0100

    add to greetings from js

--- main.js
+++ main.js
@@ -5,3 +5,9 @@ alert("Cześć");
 console.log(`Mam na imie ${name} i mam ${age} lat.`);
 const emptyParagraph = document.querySelector('.week-summary__title--js');
 emptyParagraph.innerHTML = `Nawet uzupełniłem treśc javaScriptem`;
+
+const greet = (age, firstname) =>  {
+    console.log(`Witaj drogi odwiedzający, nazwyam się ${firstname} i mam ${age} lat. `);
+}
+
+greet (46,'Przemek');
